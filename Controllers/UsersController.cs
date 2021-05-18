74c81d28448681cd8659893b4a026b85aa0f41b3
unknown <Lenovo@LRLPT-115.CORP.LOYLTY.COM>
2021-05-18 20:18:59 +0530

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await context.Users.ToListAsync();
+
+            //return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            var users = context.Users.Find(id);
+
+            return users;
+        }
+    }
+}
